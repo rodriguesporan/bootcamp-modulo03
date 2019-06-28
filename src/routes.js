0625676ffae4f02d33649771c45a4d45e4604ec7
rodriguesporan <rodriguesporan@gmail.com>
2019-06-28 11:50:18 -0300

    Upload de avatar

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+
 import authMiddleware from './app/middlewares/auth';
 
+const upload = multer(multerConfig);
 const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
