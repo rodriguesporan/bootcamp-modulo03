421125535eb8ca671cb068047c309af177aea239
rodriguesporan <rodriguesporan@gmail.com>
2019-07-01 13:52:30 -0300

    Buscando agenda de prestadores

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const upload = multer(multerConfig);
 const routes = new Router();
@@ -22,5 +23,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
