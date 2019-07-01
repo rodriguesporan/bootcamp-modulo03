acd1c8dfd57adcd4129d2ff626bd6e63e89f3741
rodriguesporan <rodriguesporan@gmail.com>
2019-07-01 08:31:34 -0300

    Definida a rota e método para criar agendamentos

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -2,7 +2,7 @@ import Sequelize, { Model } from 'sequelize';
 
 class Appointment extends Model {
   static init(sequelize) {
-    this.super(
+    super.init(
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
@@ -14,7 +14,7 @@ class Appointment extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'user_id ', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
