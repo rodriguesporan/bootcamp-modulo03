f0b278bdf9a26b96cde7b76a0bc16df5a50edb9e
rodriguesporan <rodriguesporan@gmail.com>
2019-07-04 15:13:44 -0300

    Configurado template de emails com o handlebars

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,8 +3,8 @@ export default {
   port: 2525,
   secure: false,
   auth: {
-    user: 'b72a9210870f7e',
-    pass: 'f4b87b790d6f55',
+    user: '6e99b335e75a69',
+    pass: 'fbe3d08685346a',
   },
   default: {
     from: 'Equipe GoBarber <rodriguesporan@gmail.com>',
