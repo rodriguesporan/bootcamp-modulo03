3b3acf8ce59867acc9c46f63eebfa37c489a3ac2
rodriguesporan <rodriguesporan@gmail.com>
2019-07-04 19:35:05 -0300

    Listando horários disponíveis para provedores

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,11 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
-import authMiddleware from './app/middlewares/auth';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const upload = multer(multerConfig);
 const routes = new Router();
@@ -22,6 +23,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
